82c72df87300a3e6c490698e3865c80bb6ebbc28
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-04-18 10:04:21 -0500

    first commit

---
+++ codeblog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from codeblog.models import Category, Post
+
+# Create your views here.
+def frontpageView(request):
+    posts = Post.objects.all()
+    context={
+        'posts_set':posts
+    }
+    return render(request, 'frontpage.html', context)
+
+def detailView(request, slug):
+    post=Post.objects.get(slug=slug)
+    context={
+        'post_detail':post
+    }
+    return render(request,'detail.html', context)
